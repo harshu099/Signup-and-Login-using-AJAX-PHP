1509fc0c425376ba3dea5b52483b96b81442da4b
Kumar Harsh <harshkumar12799@gmail.com>
2023-01-06 12:03:20 +0530

    all working done

--- login.php
+++ login.php
@@ -1,34 +1,42 @@
 <?php
 require 'function.php';
-if(isset($_SESSION["id"])){
+if (isset($_SESSION["id"])) {
     header("Location: index.php");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="design.css">
 </head>
-<form action="login1.php" method="post" id="login_form">
-    <input type="hidden" id="action" value="login">
+
+<body>
+
+
+    <div id="heading">PLEASE LOGIN!!!</div>
+    <form action="login1.php" method="post" id="login_form">
+        <input type="hidden" id="action" value="login">
         <div>
             <label>UserName:</label>
-            <input type="text"  id="username">
+            <input type="text" id="username">
         </div>
         <div>
             <label>Password:</label>
             <input type="text" id="password">
         </div>
         <div>
-        <button type="button" id="submit_btn" onclick="submitData();">Login</button>
+            <button type="button" id="submit_btn" onclick="submitData();">Login</button>
         </div>
         <div>
             <a href="signup.php">Go To Register</a>
         </div>
-</form>
-
+    </form>
+</body>
 <?php require './script.php'; ?>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,19 +1,25 @@
 <?php
 require 'function.php';
-if(isset($_SESSION["id"])){
+if (isset($_SESSION["id"])) {
     header("Location: index.php");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SignUp</title>
+    <link rel="stylesheet" href="design.css">
 </head>
 
-<form method="post" id="signup_form">
+<body>
+
+
+    <div id="heading">SIGN UP!!!</div>
+    <form method="post" id="signup_form">
         <input type="hidden" id="action" value="signup">
         <div>
             <label>Name:</label>
@@ -21,19 +27,21 @@ if(isset($_SESSION["id"])){
         </div>
         <div>
             <label>UserName:</label>
-            <input type="text"  id="username">
+            <input type="text" id="username">
         </div>
         <div>
             <label>Password:</label>
             <input type="text" id="password">
         </div>
         <div>
-            <button type="button" id="submit_btn" onclick="submitData();">SignUp</button>
+            <button type="button" id="submit_btn" onclick="submitData();">Sign Up</button>
         </div>
         <div>
             <a href="login.php">Go To Login</a>
         </div>
-</form>
+    </form>
+</body>
 
 <?php require './script.php'; ?>
+
 </html>
